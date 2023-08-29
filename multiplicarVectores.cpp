0bbdb79e315bcb0b318fcb7739ed20c2b66f4a8c
nana <135185645+evanessscence@users.noreply.github.com>
2023-08-28 18:20:01 -0600

    Ahora funciona
    Falta acomodar los ciclos for y los c out para que no aparezcan duplicados.

--- multiplicarVectores.cpp
+++ multiplicarVectores.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<windows.h>
 using namespace std;
 
 int main()
@@ -7,6 +7,7 @@ int main()
   int tam;
   int filas;
   
+  SetConsoleOutputCP(CP_UTF8);
   cout << "> Ingrese el tamaño del vector: "; cin >> tam;
   cout << "> Ingrese el número de filas de la matriz: "; cin >> filas;
   
@@ -34,36 +35,47 @@ int main()
     cout << endl;
 	}
    }
-
+	
+	cout << "> MATRIZ INGRESADA" << endl;
 	for (int p=0;p<filas;p++)
 	{
 	for (int s=0;s<tam;s++)
 	{
 		cout <<matriz[p][s]<<"	";
 	}
-	cout << endl;
+	cout << "\n\n";
 	}
 	
-	for (int p=0;p<filas;p++)
+	for (int p=0;p<filas-1;p++)
 	{
 	for (int s=0;s<tam-1;s++)
 	{
 		cout << vector1[p] << " x " << matriz[p][s] << " = " << vector1[p]*matriz[p][s] << "		";
 		cout << vector1[p] << " x " << matriz[p+1][s] << " = " << vector1[p]*matriz[p+1][s] << "		" << endl;
-		cout << vector1[p+1] << " x " << matriz[p][s+1] << " = " << vector1[p]*matriz[p+1][s] << "		";
-		cout << vector1[p+1] << " x " << matriz[p+1][s+1] << " = " << vector1[p]*matriz[p+1][s+1] << "		" << endl;
 		vectorMultiplicado[s] = vector1[p]*matriz[p][s] + vector1[p]*matriz[p+1][s];
+		cout << vectorMultiplicado[p] << endl;
+	}	
+	cout << "\n\n";
+	}
+	
+	for (int p=0;p<filas-1;p++)
+	{
+	for (int s=0;s<tam-1;s++)
+	{
+		cout << vector1[s+1] << " x " << matriz[p][s+1] << " = " << vector1[p+1]*matriz[p][s+1] << "		";
+		cout << vector1[s+1] << " x " << matriz[p+1][s+1] << " = " << vector1[p+1]*matriz[p+1][s+1] << "		" << endl;
+		vector2[s] = vector1[p+1]*matriz[p][s+1] + vector1[p+1]*matriz[p+1][s+1];
+		
 	}	
 	cout << endl;
 	}
    
-   cout << endl << "> Vector multiplicado: " << endl;
-   cout << "<";
+   cout << endl << "> Vector sumado: " << endl;
    
    for (int d=0;d<tam;d++)
   {
-    cout << vectorMultiplicado[d] << ", ";
+    cout << vectorMultiplicado[d];
+    cout << "	" << vector2[d] << endl;
    }
-   
-   cout << ">";
+
 return 0; }
